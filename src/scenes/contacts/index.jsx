17d5a2f703b9adc0fae5b63a1c73f3b6d758ad17
RickDeb2004 <debanjanrick04@gmail.com>
2024-06-27 12:13:54 +0530

    fixed lamp effect in form

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -272,7 +272,7 @@ const Contacts = () => {
             "&::before": {
               content: '""',
               position: "absolute",
-              bottom: 0,
+              top: 0,
               left: 0,
               width: "100%",
               height: "10px",
